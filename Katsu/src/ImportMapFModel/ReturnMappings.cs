533ad6c486f6b1facad3c8cb020162ac36789760
Lex <115362195+iLexyyy@users.noreply.github.com>
2022-10-23 22:51:16 +0100

    Add files via upload

---
+++ Katsu/src/ImportMapFModel/ReturnMappings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katsu.src.FModel
+{
+    class ReturnMappings
+    {
+        public string FModelPathData { get; set; }
+    }
+}
